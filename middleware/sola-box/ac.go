7d2ca98f3451df85f86bfda5e1a20350c01e6c2f
liujun <1173727203@qq.com>
2019-12-05 15:26:25 +0800

    ac_user

--- middleware/sola-box/ac.go
+++ middleware/sola-box/ac.go
@@ -2,6 +2,7 @@ package box
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/ddosakura/sola/v2/middleware/logger"
 
@@ -74,11 +75,19 @@ func AC(db *gorm.DB, key string) (sola.Middleware, *router.Router, RequestAC) {
 			return c.String(http.StatusForbidden, "Forbidden")
 		})
 	}
+
+	acr1 := ACR(ac.TypeRole, ac.LogicalOR, "admin")
+	r.BindFunc("GET /user/:id", requestAC(acr1, getUser))
+	r.BindFunc("GET /user", requestAC(acr1, getUsers))
+	r.BindFunc("DELETE /user/:id", requestAC(acr1, delUser))
+	r.BindFunc("POST /user", requestAC(acr1, postUser))
+	r.BindFunc("PUT /user/:id", requestAC(acr1, putUser))
+
 	return init, r, requestAC
 }
 
-// ReqLogin Form
-type ReqLogin struct {
+// ReqUser Form
+type ReqUser struct {
 	Username string
 	Password string
 }
@@ -87,7 +96,7 @@ func login(next sola.Handler) sola.Handler {
 	return func(c sola.Context) error {
 		s := c.Get(CtxBoxAC).(*ac.Srv)
 
-		var a ReqLogin
+		var a ReqUser
 		if err := c.GetJSON(&a); err != nil {
 			return err
 		}
@@ -146,6 +155,7 @@ func userInfo(c sola.Context) error {
 	})
 }
 
+// TODO: need change to getJSON
 func register(c sola.Context) error {
 	s := c.Get(CtxBoxAC).(*ac.Srv)
 	r := c.Request()
@@ -160,4 +170,85 @@ func register(c sola.Context) error {
 		return success(c)
 	}
 	return fail(c)
+}
+
+// CRUD
+
+func acSucc(c sola.Context, v interface{}) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"code": 0,
+		"msg":  "SUCCESS",
+		"data": v,
+	})
+}
+
+func getUsers(c sola.Context) error {
+	s := c.Get(CtxBoxAC).(*ac.Srv)
+	r := c.Request()
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil {
+		page = 1
+	}
+	size, err := strconv.Atoi(r.URL.Query().Get("size"))
+	if err != nil {
+		size = 5
+	}
+	list := s.SelectUserList(page, size)
+	return acSucc(c, list)
+}
+
+func getUser(c sola.Context) error {
+	s := c.Get(CtxBoxAC).(*ac.Srv)
+	id, err := strconv.Atoi(router.Param(c, "id"))
+	if err != nil {
+		return err
+	}
+	u := s.SelectUser(uint(id))
+	return acSucc(c, u)
+}
+
+func delUser(c sola.Context) error {
+	s := c.Get(CtxBoxAC).(*ac.Srv)
+	id, e := strconv.Atoi(router.Param(c, "id"))
+	if e != nil {
+		return fail(c)
+	}
+	s.DeleteUser(id)
+	return acSucc(c, nil)
+}
+
+func postUser(c sola.Context) error {
+	s := c.Get(CtxBoxAC).(*ac.Srv)
+	var a ReqUser
+	if e := c.GetJSON(&a); e != nil {
+		return e
+	}
+	if e := s.PostUser(a.Username, a.Password); e != nil {
+		return fail(c)
+	}
+	return acSucc(c, nil)
+}
+
+// UserInfo -
+type UserInfo struct {
+	Password string
+	Nick     string
+	Avatar   string
+	Desc     string
+}
+
+func putUser(c sola.Context) error {
+	s := c.Get(CtxBoxAC).(*ac.Srv)
+	var a UserInfo
+	if e := c.GetJSON(&a); e != nil {
+		return fail(c)
+	}
+	id, e := strconv.Atoi(router.Param(c, "id"))
+	if e != nil {
+		return fail(c)
+	}
+	if e := s.PutUser(id, a.Nick, a.Password, a.Avatar, a.Desc); e != nil {
+		return fail(c)
+	}
+	return acSucc(c, nil)
 }
